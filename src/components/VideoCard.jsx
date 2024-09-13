43bb483c7b05d1572646383319492c1690af4d17
ACJT123 <acjtacjt@gmail.com>
2024-09-13 22:40:23 +0800

    feat(dark-mode): update component theme

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,23 +1,52 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import "../pages/feed.css"
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../pages/feed.css";
+import { useSelector } from "react-redux";
+
 function VideoCard(props) {
-  const pageRoute = useNavigate()
+  const pageRoute = useNavigate();
+  const { darkMode } = useSelector((state) => state.darkMode);
   return (
-    <div style={{ width: props.width, display: props.display }} className='w-[100%] sm:w-[90%] md:w-[100%] relative cursor-pointer videoComponent'>
-      <img onClick={() => pageRoute(`/watch/${props.videoId}`)} className='md:w-56 lg:w-72 rounded-[12px] videoImage' src={props.thumbnail} />
-      <div style={{ width: props.rightWidth }} className='flex w-[100%] gap-x-3 items-start mt-2'><div>
-        <h3 onClick={() => pageRoute(`/watch/${props.videoId}`)} className='text-[14px] lg:text-[16px] font-semibold leading-[20px] w-[94%]'>
-          {props.title?.slice(0, 60)}
-        </h3>
-        <div className='mt-1'>
-          <p onClick={() => pageRoute(`/channel/${props.channelId}`)} className='text-[11.5px] text-[#606060] lg:text-[13.5px] font-[500] tracking-wide'>{props.channel}</p>
-          <p onClick={() => pageRoute(`/watch/${props.videoId}`)} className='text-[11.5px] text-[#606060] lg:text-[13.5px] font-medium tracking-wider -mt-1'>{props.on}</p>
+    <div
+      style={{ width: props.width, display: props.display }}
+      className="w-[100%] sm:w-[90%] md:w-[100%] relative cursor-pointer videoComponent"
+    >
+      <img
+        onClick={() => pageRoute(`/watch/${props.videoId}`)}
+        className="md:w-56 lg:w-72 rounded-[12px] videoImage"
+        src={props.thumbnail}
+      />
+      <div
+        style={{ width: props.rightWidth }}
+        className="flex w-[100%] gap-x-3 items-start mt-2"
+      >
+        <div>
+          <h3
+            onClick={() => pageRoute(`/watch/${props.videoId}`)}
+            className={`text-[14px] lg:text-[16px] font-semibold leading-[20px] w-[94%] ${
+              darkMode && "text-black"
+            }`}
+          >
+            {props.title?.slice(0, 60)}
+          </h3>
+          <div className="mt-1">
+            <p
+              onClick={() => pageRoute(`/channel/${props.channelId}`)}
+              className="text-[11.5px] text-[#606060] lg:text-[13.5px] font-[500] tracking-wide"
+            >
+              {props.channel}
+            </p>
+            <p
+              onClick={() => pageRoute(`/watch/${props.videoId}`)}
+              className="text-[11.5px] text-[#606060] lg:text-[13.5px] font-medium tracking-wider -mt-1"
+            >
+              {props.on}
+            </p>
+          </div>
         </div>
       </div>
-      </div>
     </div>
-  )
+  );
 }
 
-export default VideoCard
+export default VideoCard;
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,38 +1,63 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import VideoCard from '../components/VideoCard'
-import { getCategoryVideos } from '../redux/categorySlice'
-import { useDispatch, useSelector } from 'react-redux'
-import timeSince from '../utils/date'
-import './feed.css'
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import VideoCard from "../components/VideoCard";
+import { getCategoryVideos } from "../redux/categorySlice";
+import { useDispatch, useSelector } from "react-redux";
+import timeSince from "../utils/date";
+import "./feed.css";
 function Feed() {
-  const { id } = useParams()
-  const dispatch = useDispatch()
-  const { categoryVideos } = useSelector((state) => state.category)
-  const { sidebarExtend } = useSelector((state) => state.category)
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const { categoryVideos } = useSelector((state) => state.category);
+  const { sidebarExtend } = useSelector((state) => state.category);
+  const { darkMode } = useSelector((state) => state.darkMode);
+
   useEffect(() => {
-    dispatch(getCategoryVideos(`search?part=snippet&q=${id ? id : "Coding development"}`))
-    document.title = `${id ? id + "- Youtube" : "Home - Youtube"}`
-  }, [id])
+    dispatch(
+      getCategoryVideos(
+        `search?part=snippet&q=${id ? id : "Coding development"}`
+      )
+    );
+    document.title = `${id ? id + "- Youtube" : "Home - Youtube"}`;
+  }, [id]);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = darkMode ? "#131417" : "#fff";
+  }, [darkMode]);
   var aDay = 24 * 60 * 60 * 1000;
   return (
     <>
       {/* <Sidebar /> */}
-      <div className={`sm:hidden overlayEffect ${sidebarExtend ? "block" : "hidden"}`}></div>
-      <div className={`pl-0  ${sidebarExtend ? "sm:pl-[180px]" : "sm:pl-[70px]"} feedGrid grid sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-x-[4%] pt-20 mx-3 sm:ml-4 md:pr-[28px] lg:pr-14 gap-y-6 max-w-[100%] bg-contain `}>
-
-        {
-          categoryVideos?.map((e, index) => {
-            return (
-              <div key={index} style={{ marginTop: index === 0 ? "0px" : "0px" }}>
-                <VideoCard key={index} title={e.snippet.title} thumbnail={e.snippet?.thumbnails?.medium?.url} on={timeSince(new Date(Date.parse(e.snippet.publishedAt) - aDay))} channel={e.snippet.channelTitle} channelId={e.snippet.channelId} videoId={e.id.videoId} />
-              </div>
-            )
-          })
-        }
+      <div
+        className={`sm:hidden overlayEffect ${
+          sidebarExtend ? "block" : "hidden"
+        }`}
+      ></div>
+      <div
+        className={`pl-0  ${
+          sidebarExtend ? "sm:pl-[180px]" : "sm:pl-[70px]"
+        } feedGrid grid sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-x-[4%] pt-20 mx-3 sm:ml-4 md:pr-[28px] lg:pr-14 gap-y-6 max-w-[100%] bg-contain `}
+      >
+        {categoryVideos?.map((e, index) => {
+          return (
+            <div key={index} style={{ marginTop: index === 0 ? "0px" : "0px" }}>
+              <VideoCard
+                key={index}
+                title={e.snippet.title}
+                thumbnail={e.snippet?.thumbnails?.medium?.url}
+                on={timeSince(
+                  new Date(Date.parse(e.snippet.publishedAt) - aDay)
+                )}
+                channel={e.snippet.channelTitle}
+                channelId={e.snippet.channelId}
+                videoId={e.id.videoId}
+              />
+            </div>
+          );
+        })}
       </div>
     </>
-  )
+  );
 }
 
-export default Feed
+export default Feed;
